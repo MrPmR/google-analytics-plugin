dcbebf7ec4822d9cca44dc1e63f0b143e519def5
Pierre-Luc Morneau <plmorneau@emappetizer.com>
2016-05-09 10:47:53 -0400

    Added a basic of track event

--- windows/UniversalAnalyticsProxy.js
+++ windows/UniversalAnalyticsProxy.js
@@ -1,7 +1,8 @@
 cordova.commandProxy.add("UniversalAnalytics",{
     startTrackerWithId:function(successCallback, errorCallback, trackingId) {
+        trackingId = JSON.stringify(trackingId);
         try {
-            var res = UniversalAnalyticsRuntimeComponent.UniversalAnalytics.default.startTrackerWithId(trackingId);
+            var res = UniversalAnalyticsRuntimeComponent.UniversalAnalytics.def.startTrackerWithId(trackingId);
             successCallback();
             
         } catch (error) {
@@ -9,5 +10,18 @@ cordova.commandProxy.add("UniversalAnalytics",{
             errorCallback(res);
         }
         
+    },
+    trackEvent: function (successCallback, failCallback, args)
+    {
+        args = JSON.stringify(args);
+        try {
+            var res = UniversalAnalyticsRuntimeComponent.UniversalAnalytics.def.trackEvent(args);
+            successCallback();
+        }
+        catch (error)
+        {
+            console.log("Error while tracking event : " + error);
+            errorCallback(res);
+        }
     }
 });
