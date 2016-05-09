d71dbd74309a599d03b64f962eb85876f023d615
Pierre-Luc Morneau <plmorneau@emappetizer.com>
2016-05-09 09:03:56 -0400

    fix a mistake (forgot this)

--- windows/UniversalAnalyticsProxy.js
+++ windows/UniversalAnalyticsProxy.js
@@ -2,11 +2,11 @@ cordova.commandProxy.add("UniversalAnalytics",{
     tracker: null,
     startTrackerWithId:function(successCallback, errorCallback, strInput) {
         var UA = strInput[0];
-        if(this.tracker == null)
+        if(this.UA == null)
         {
             this.UA = new UniversalAnalyticsRuntimeComponent.UniversalAnalytics();
         }
-        var res =UA.startTrackerWithId(trackingId);
+        var res = this.UA.startTrackerWithId(trackingId);
         if(res.indexOf("Error") == 0) {
             errorCallback(res);
         }
