7938b3d83d2be760327286f46a0724ace46186ab
Pierre-Luc Morneau <plmorneau@emappetizer.com>
2016-05-09 11:55:42 -0400

    Removed references to winmd as a test

--- windows/UniversalAnalyticsProxy.js
+++ windows/UniversalAnalyticsProxy.js
@@ -1,27 +1,54 @@
 cordova.commandProxy.add("UniversalAnalytics",{
-    startTrackerWithId:function(successCallback, errorCallback, trackingId) {
-        trackingId = JSON.stringify(trackingId);
-        try {
-            var res = UniversalAnalyticsRuntimeComponent.UniversalAnalytics.def.startTrackerWithId(trackingId);
-            successCallback();
+    startTrackerWithId:function(successCallback, failCallback, args) {
+        if(args.length < 0)
+            failCallback("Bad tracking Id");
+        var config = new GoogleAnalytics.EasyTrackerConfig();
+        config.trackingId = args[0];
+        // TODO: Get real app name/version
+        config.appName = "Test App";
+        config.appVersion = "1.0.0.0";
+        GoogleAnalytics.EasyTracker.current.config = config;
+        // trackingId = JSON.stringify(trackingId);
+        // try {
+        //     var res = UniversalAnalyticsRuntimeComponent.UniversalAnalytics.def.startTrackerWithId(trackingId);
+        //     successCallback();
             
-        } catch (error) {
-            console.log("Error while starting tracker : " + error);
-            errorCallback(res);
-        }
+        // } catch (error) {
+        //     console.log("Error while starting tracker : " + error);
+        //     errorCallback(res);
+        // }
         
     },
     trackEvent: function (successCallback, failCallback, args)
     {
-        args = JSON.stringify(args);
-        try {
-            var res = UniversalAnalyticsRuntimeComponent.UniversalAnalytics.def.trackEvent(args);
-            successCallback();
-        }
-        catch (error)
+        switch(args.length)
         {
-            console.log("Error while tracking event : " + error);
-            errorCallback(res);
+            case 0:
+                failCallback("Expected at least two arguments");
+                break;
+            case 1:
+                failCallback("Expected at least two arguments");
+                break;
+            case 2:
+                GoogleAnalytics.EasyTracker.getTracker().sendEvent(args[0], args[1]);
+                break;
+            case 3:
+                GoogleAnalytics.EasyTracker.getTracker().sendEvent(args[0], args[1], args[2]);
+                break;
+            default:
+                GoogleAnalytics.EasyTracker.getTracker().sendEvent(args[0], args[1], args[2], args[3]);
+                break;
         }
+        successCallback();
+        // args = JSON.stringify(args);
+        // try {
+        //     var res = UniversalAnalyticsRuntimeComponent.UniversalAnalytics.def.trackEvent(args);
+        //     successCallback();
+        // }
+        // catch (error)
+        // {
+        //     console.log("Error while tracking event : " + error);
+        //     errorCallback(res);
+        // }
     }
 });
