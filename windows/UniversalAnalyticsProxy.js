f8678c14fb8c4d50ae9296d5fb13ded3f5269366
Pierre-Luc Morneau <plmorneau@emappetizer.com>
2016-05-09 09:58:52 -0400

    added the default and success/fail callbacks

--- windows/UniversalAnalyticsProxy.js
+++ windows/UniversalAnalyticsProxy.js
@@ -1,10 +1,12 @@
 cordova.commandProxy.add("UniversalAnalytics",{
     startTrackerWithId:function(successCallback, errorCallback, trackingId) {
         try {
-            var res = UniversalAnalyticsRuntimeComponent.UniversalAnalytics.startTrackerWithId(trackingId);
+            var res = UniversalAnalyticsRuntimeComponent.UniversalAnalytics.default.startTrackerWithId(trackingId);
+            successCallback();
             
         } catch (error) {
             console.log("Error while starting tracker : " + error);
+            errorCallback(res);
         }
         
     }
